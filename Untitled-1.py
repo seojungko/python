eb8e584343eb47b2e59f493a7220e7c38ae93de1
seojung <seojung.ko@gmail.com>
2018-10-20 21:49:50 -0700

    first try to use github

---
+++ Untitled-1.py
@@ -0,0 +1,3 @@
+print('Hello World!')
+my_dic={'install python':'completed', 'install git':'completed', 'install github':'completed', 'fork github':'completed','homework#1':'not yet','homework#2':'not yet'}
+print(my_dic)
